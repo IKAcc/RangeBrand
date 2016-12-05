ba46c78da1436bc79317d18600ba0c207cec657f
Javid Izadfar <javid.izadfar@gmail.com>
2016-12-05 23:01:33 +0330

    fixed glassify

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
+function glass(element, amount){
+
+  var regEx = /(rgb)\(([0-9]+),\s+([0-9]+),\s+([0-9]+)/;
+  var currentColor = $(element).css('background-color');
+  var str = 'rgba($2,$3,$4,' + (amount / 100).toString();
+  var newColor = currentColor.replace(regEx, str);
+  return newColor;
+}
+
 (function( $ ) {
 
   $('.donate').on('click', function(event) {
